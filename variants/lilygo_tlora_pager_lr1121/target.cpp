f5d8a4734c45460943b1335e943d74b5944eb744
CJ van Soest <CJvanSoest@users.noreply.github.com>
2026-09-18 14:48:57 +0000

    Match repo's existing halt-loop style

--- variants/lilygo_tlora_pager_lr1121/target.cpp
+++ variants/lilygo_tlora_pager_lr1121/target.cpp
@@ -557,7 +557,8 @@ void TLoraPagerBoard::powerOff() {
   Serial.flush();
   delay(100);
   tpager_power_off();
-  while (1); // rail should already be dead; wait it out just in case
+  // Rail should already be dead. Wait it out just in case.
+  while (1) ;
 }
 
 static int read_bq27220_word(uint8_t reg) {
